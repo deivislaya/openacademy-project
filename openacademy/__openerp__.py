03999738f47507cbdc95d0fd25ca61c82daa5f8e
Deivis Laya <deivis@vauxoo.com>
2016-02-04 19:28:19 +0000

    [REF] openacademy: Add data demo

--- openacademy/__openerp__.py
+++ openacademy/__openerp__.py
@@ -26,6 +26,7 @@
     # only loaded in demonstration mode
     'demo': [
         # 'demo.xml',
+        'demo/openacademy_course_demo.xml',
     ],
     'installable': True,
     'auto_install': False,
